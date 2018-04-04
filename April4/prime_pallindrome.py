429e0b25f1e4fa499c7c0c36c8e1d3adc16e5b53
Akashbhaskaran <akaashbhaskaran@gmail.com>
2018-04-04 16:17:11 +0530

    Add files via upload

---
+++ April4/prime_pallindrome.py
@@ -0,0 +1,29 @@
+lower = 900
+upper = 1000
+
+# uncomment the following lines to take input from the user
+#lower = int(input("Enter lower range: "))
+#upper = int(input("Enter upper range: "))
+
+print("Prime numbers between",lower,"and",upper,"are:")
+
+for num in range(lower,upper + 1):
+   # prime numbers are greater than 1
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+           temp = num
+           rev = 0
+
+           while temp != 0:
+               rev = (rev * 10) + (temp % 10)
+               temp = temp // 10
+
+           if num == rev:
+               print("number is palindrome")
+           
+
+

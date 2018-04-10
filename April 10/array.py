95617811807cdf539a996d5a9a3dd13c1c21f12f
Akashbhaskaran <akaashbhaskaran@gmail.com>
2018-04-10 20:15:16 +0530

    Add files via upload

---
+++ April 10/array.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 18:36:35 2018
+
+@author: user
+"""
+"""import numpy as np
+A = [1,2,3,4,5,6,7,8,9,10,11,12]
+B = np.reshape(A, (-1, 2))
+print(type(B))
+print(B)
+
+C = np.reshape(B, (-1, 3))
+
+"""
+a = np.zeros(shape=(4,4))
+print(a)
+b = a.reshape(4,2,2)
+print(b)
+a=a+5
+b=b+5
+print(a)
+print(b)
+a=a-2
+b=b-2
+print(a)
+print(b)
+a=a*6
+b=b*6
+print(a)
+print(b)
+
